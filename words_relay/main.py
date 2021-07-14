d8c5233d5d522e7b6ff572db1466537e68e681df
Forgot <80463183+Forgot09@users.noreply.github.com>
2021-07-14 19:09:22 +0900

    version 0.2.5
    - fix many error
    - realy realy realy all error fixed!
    
    - 많은 오류가 고쳐졌습니다
    - 진짜 진짜 진짜 모든 오류가 고쳐졌다고요!

--- words_relay/main.py
+++ words_relay/main.py
@@ -67,37 +67,37 @@ def bot_words(data):
     global wordsList, botWords, dum, bunli, dum_etc, canFirstWords
     turn = 0
     checkVar = True
-    if data[-1]+'\n' in canFirstWords or dum[data[-1]]+'\n' in canFirstWords or bunli[1] == 'ㄴ' and dum_etc[data[-1]+'\n' in canFirstWords]:
-        if not data[-1] in dum or not bunli[2] == 'ㄴ' and data[-1] in dum_etc:
-            while checkVar:
-                botWords = wordsList[turn]
-                if data[-1] == botWords[0]:
-                    botWords = botWords.rstrip('\n')
-                    print(botWords)
-                    wordsList.remove(botWords+'\n')
-                    checkVar = False
-                turn += 1
-        elif data[-1] in dum:
+    if bunli[2] == 'ㄴ' and data[-1] in dum_etc:
+        while checkVar:
+            botWords = wordsList[turn]
+            if dum_etc[data[-1]] == botWords[0]:
+                print(botWords, end='')
+                wordsList.remove(botWords)
+                checkVar = False
+            turn += 1
+    
+    elif data[-1] in dum:
             while checkVar:
                 botWords = wordsList[turn]
                 if dum[data[-1]] == botWords[0]:
-                    botWords = botWords.rstrip('\n')
-                    print(botWords)
-                    wordsList.remove(botWords+'\n')
+                    print(botWords, end='')
+                    wordsList.remove(botWords)
                     checkVar = False
                 turn += 1
-        elif bunli[2] == 'ㄴ' and data[-1] in dum_etc:
-            while checkVar:
-                if dum_etc[data[-1]] == botWords[0]:
-                    botWords = botWords.rstrip('\n')
-                    print(botWords)
-                    wordsList.remove(botWords+'\n')
-                    checkVar = False
-
     else:
-        print('[!] 당신의 승리입니다. 축하드립니다 이 화면을 캡쳐하여 개발자 이메일로 전해주세요')
-        print('[!] 개발자 이메일 >> forcoding4@gmail.com')
-        sys.exit('[!] 끝말잇기가 종료 되었습니다')
+        if not bunli[2] == 'ㄴ' and data[-1] in dum_etc:
+            if not data[-1] in dum:
+                while checkVar:
+                    botWords = wordsList[turn]
+                    if data[-1] == botWords[0]:
+                        print(botWords, end='')
+                        wordsList.remove(botWords)
+                        checkVar = False
+                    turn += 1
+        else:
+            print('[!] 당신의 승리입니다. 축하드립니다 이 화면을 캡쳐하여 개발자 이메일로 전해주세요')
+            print('[!] 개발자 이메일 >> forcoding4@gmail.com')
+            sys.exit('[!] 끝말잇기가 종료 되었습니다')
 #####################################################
 
 ###################   main   ########################
@@ -120,6 +120,7 @@ while judgement:
             judgement = True
             
     else:
+        botWords = botWords.rstrip('\n')
         inputData = input('[!] 단어를 입력해주세요 >> ')
         inputData = inputData.rstrip('\n')
         bunli = jamo(inputData)
@@ -136,7 +137,6 @@ while judgement:
     try:
         bot_words(inputData) 
         turnTimes += 1
-    except IndexError:
-        print('[!] 당신의 승리입니다. 축하드립니다 이 화면을 캡쳐하여 개발자 이메일로 전해주세요')
+    except:
+        print('[!] 예상치 못한 오류가 발생한것 같습니다, 개발자에게 연락하세요')
         print('[!] 개발자 이메일 >> forcoding4@gmail.com')
-        sys.exit('[!] 끝말잇기가 종료 되었습니다')
